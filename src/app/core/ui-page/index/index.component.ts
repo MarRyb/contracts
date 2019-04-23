3538cc8654cf5909e407a263a55f38b368d3bc25
Marina <mg@health24.ua>
2019-04-23 20:29:52 +0300

    added five point's

--- src/app/core/ui-page/index/index.component.ts
+++ src/app/core/ui-page/index/index.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { DataContractsService } from 'src/app/core/api/data-contracts.service';
+import { FormsModule }   from '@angular/forms';
 
 @Component({
   selector: 'app-index',
@@ -8,21 +9,38 @@ import { DataContractsService } from 'src/app/core/api/data-contracts.service';
 })
 export class IndexComponent implements OnInit {
   public dataContr: any; 
-  public metaData: any;
-  public rows: any;
-
+  public newObject: any;
+  public newContract: any = {};
+ 
   constructor(public dataContractsService: DataContractsService) { 
     dataContractsService.getList().subscribe(data => {
       this.dataContr = data;
-
-      this.metaData = this.dataContr.data.metaData
-      this.rows = this.dataContr.data.rows
-      console.log(this.metaData)
-      console.log(this.rows)
-      debugger
-
-      return this.dataContr;
+      var metaData = this.dataContr.data.metaData
+      var rows = this.dataContr.data.rows
+      this.newObject = rows.map(function(num) {
+        var result = {};
+        metaData.forEach((key, i) => result[key.name] = num[i]);
+        return result;
+      });
+      return this.newObject;
     })
+
+  }
+
+  delItem(item, index) {
+    this.newObject.splice(index, 1);
+    return this.newObject;
+  }
+
+  addItemCopy(item, index) {
+    this.newObject.push(item);
+    return this.newObject;
+  }
+
+
+  addItem() {
+    this.newObject.push(Object.assign({}, this.newContract));
+    return this.newObject;
   }
 
   ngOnInit() {
--- src/app/core/ui-page/ui-page.module.ts
+++ src/app/core/ui-page/ui-page.module.ts
@@ -2,11 +2,13 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { CreateComponent } from './create/create.component';
 import { IndexComponent } from './index/index.component';
+import { FormsModule } from '@angular/forms';
 
 @NgModule({
   declarations: [CreateComponent, IndexComponent],
   imports: [
-    CommonModule
+    CommonModule,
+    FormsModule
   ],
   exports: [CreateComponent, IndexComponent]
 })
